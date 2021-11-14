77ddb9e6a6a851c8b1e720773f7a346a00e98737
Tommy Zarick <zarick.6@osu.edu>
2021-11-13 21:40:36 -0500

    setup - read input

--- gophercises/quizgame/main.go
+++ gophercises/quizgame/main.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	probs, count, err := readProblems("problems.csv")
+	problemsCsvFilename := flag.String("problemsCsvFilename", "problems.csv", "The filename of the csv holding the problems for the quiz")
+	flag.Parse()
+
+	probs, err := readProblems(problemsCsvFilename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("read %d bytes: %q\n", count, (*probs)[:count])
+	// fmt.Printf("read %d bytes: %q\n", count, (*probs)[:count])
+	fmt.Println(probs)
+	// for i := 0; i < count; i++ {
+	// 	fmt.Printf("%q", (*probs)[i])
+	// }
 }
 
-func readProblems(filename string) (*[]byte, int, error) {
-	file, err := os.Open(filename)
+func readProblems(filename *string) (string, error) {
+	file, err := os.Open(*filename)
 	if err != nil {
-		return &[]byte{}, 0, errors.New(err.Error())
+		return "", errors.New(err.Error())
 	}
+
+	defer file.Close()
 
 	data := make([]byte, 100)
 	count, err := file.Read(data)
 	if err != nil {
-		return &[]byte{}, 0, errors.New(err.Error())
+		return "", errors.New(err.Error())
 	}
 
-	return &data, count, nil
+	probsString := fmt.Sprintf("%q", data[:count])
+
+	return probsString, nil
 }
