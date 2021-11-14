d711c6bccb2ae4432cd93c7da11ea757a24e1d38
Tommy Zarick <zarick.6@osu.edu>
2021-11-14 11:44:57 -0500

    more progress on quizgame gophercise

--- gophercises/quizgame/main.go
+++ gophercises/quizgame/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,16 +19,28 @@ func main() {
 	}
 
 	// fmt.Printf("read %d bytes: %q\n", count, (*probs)[:count])
-	fmt.Println(probs)
+	questionAnswer := map[string]string{}
+	for _, v := range probs {
+		if len(v) > 0 {
+			questionElements := strings.Split(v, ",")
+			elementLen := len(questionElements)
+			answer := questionElements[elementLen-1]
+			question := strings.Join(questionElements[:(elementLen-1)], ",") // account for questions that have commas in them
+			questionAnswer[question] = answer
+		}
+	}
+
+	fmt.Println(questionAnswer)
+
 	// for i := 0; i < count; i++ {
 	// 	fmt.Printf("%q", (*probs)[i])
 	// }
 }
 
-func readProblems(filename *string) (string, error) {
+func readProblems(filename *string) ([]string, error) {
 	file, err := os.Open(*filename)
 	if err != nil {
-		return "", errors.New(err.Error())
+		return []string{}, errors.New(err.Error())
 	}
 
 	defer file.Close()
@@ -35,10 +48,11 @@ func readProblems(filename *string) (string, error) {
 	data := make([]byte, 100)
 	count, err := file.Read(data)
 	if err != nil {
-		return "", errors.New(err.Error())
+		return []string{}, errors.New(err.Error())
 	}
 
-	probsString := fmt.Sprintf("%q", data[:count])
+	probsString := string(data[:count])
+	problemElements := strings.Split(probsString, "\n")
 
-	return probsString, nil
+	return problemElements, nil
 }
