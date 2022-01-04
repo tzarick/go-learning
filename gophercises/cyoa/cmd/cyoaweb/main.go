c41ebc3e25d4d7b5d5cc36cba6e2c1f7397e7999
Tommy Zarick <zarick.6@osu.edu>
2022-01-03 23:35:35 -0500

    rendering

--- gophercises/cyoa/cmd/cyoaweb/main.go
+++ gophercises/cyoa/cmd/cyoaweb/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/tzarick/go-learning/gophercises/cyoa"
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	filename := flag.String("file", "gopher.json", "the JSON file with the Choose Your Own Adventurer story")
+	port := flag.Int("port", 3000, "the port to start the CYOA app on")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
 
@@ -20,11 +21,12 @@ func main() {
 		log.Fatalf("error opening file [%s]: %v", *filename, err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
-		log.Fatalf("error decoding json: %v", err)
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- gophercises/cyoa/story.go
+++ gophercises/cyoa/story.go
@@ -1,5 +1,68 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+      <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+      <li>
+        <a href="/{{.Chapter}}">{{.Text}}</a>
+      </li>
+			{{end}}
+    </ul>
+  </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, fmt.Errorf("error decoding json: %v", err)
+	}
+
+	return story, nil
+}
+
 type Story map[string]Chapter
 
 type Chapter struct {
@@ -9,6 +72,6 @@ type Chapter struct {
 }
 
 type Option struct {
-	Text string `json:"text"`
-	Arc  string `json:"arc"`
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
 }
